ae98b95ec5039670cf4e6c5c89c786f506e657bf
Nandan kumar <kumarnandan551@gmail.com>
2024-12-28 19:22:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 function convertToRoman(num) {
+    if (num < 1 || num > 100000) {
+        return "Input number out of range. Please enter a number between 1 and 100000.";
+    }
+
     // Roman numeral symbols and their corresponding values
-    const romanSymbols = [
-        ['M', 1000], 
-        ['D', 500], 
-        ['C', 100], 
-        ['L', 50], 
-        ['X', 10], 
-        ['V', 5], 
+    const romanNumerals = [
+        ['M', 1000],
+        ['D', 500],
+        ['C', 100],
+        ['L', 50],
+        ['X', 10],
+        ['V', 5],
         ['I', 1]
     ];
-    
+
     let result = '';
 
-    // Loop through each symbol
-    for (let i = 0; i < romanSymbols.length; i++) {
-        while (num >= romanSymbols[i][1]) {
-            result += romanSymbols[i][0];
-            num -= romanSymbols[i][1];
+    // Loop through each Roman numeral value
+    for (let i = 0; i < romanNumerals.length; i++) {
+        // Subtract as many of the current Roman numeral value as possible
+        while (num >= romanNumerals[i][1]) {
+            result += romanNumerals[i][0];
+            num -= romanNumerals[i][1];
         }
     }
-    
+
     return result;
 }
-
-// Example usage
-console.log(convertToRoman(14));  // Output: XIV
-console.log(convertToRoman(798)); // Output: DCCXCVIII

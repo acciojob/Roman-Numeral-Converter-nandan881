7732a574a7185a1e84c59652ebc03b373f3ea909
Nandan kumar <kumarnandan551@gmail.com>
2024-12-31 19:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 function convertToRoman(num) {
     const romanSymbols = [
-        ['M', 1000], 
-        ['D', 500], 
-        ['C', 100], 
-        ['L', 50], 
-        ['X', 10], 
-        ['V', 5], 
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
         ['I', 1]
     ];
 
     let result = '';
 
-    for (let i = 0; i < romanSymbols.length; i++) {
-        const [symbol, value] = romanSymbols[i];
+    for (const [symbol, value] of romanSymbols) {
         while (num >= value) {
             result += symbol;
             num -= value;
-        }
-
-        // Handle special cases like IV (4), IX (9), etc.
-        if (i < romanSymbols.length - 1) {
-            const [nextSymbol, nextValue] = romanSymbols[i + 1];
-            if (num >= value - nextValue && value - nextValue > nextValue) {
-                result += nextSymbol + symbol;
-                num -= (value - nextValue);
-            }
         }
     }
 
     return result;
 }
 
-// Examples
-console.log(convertToRoman(14)); // Output: XIV
-console.log(convertToRoman(798)); // Output: DCCXCVIII
+function convertAndDisplay() {
+    const input = document.getElementById('numberInput').value;
+    const outputDiv = document.getElementById('output');
+
+    if (input === '' || input < 0 || input > 100000) {
+        outputDiv.textContent = 'Please enter a valid number between 0 and 100000.';
+    } else {
+        const romanNumeral = convertToRoman(Number(input));
+        outputDiv.textContent = romanNumeral;
+    }
+}
